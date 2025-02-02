1e1b624a625a4298f0cf7b78304d2a6b4dfba23e
Tabish <mohdtabishkhan001@gmail.com>
2025-02-02 22:38:13 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,26 @@ function App() {
 
   const card_content = [{
     feature: "Resume Scoring and Optimization",
-    description: 'Analyze uploaded resumes for format, grammar, and ATS  compatibility and get tailored improvements based on specific industries, roles, or job descriptions.'
+    description: 'Analyze uploaded resumes for format, grammar, and ATS compatibility and get tailored improvements based on specific industries, roles, or job descriptions.'
+  },
+  {
+    feature: "Resources with Question banks.",
+    description: 'Offer a curated library of common and role-specific interview questions. Enable a practice mode where users can attempt these questions and get feedback.'
+  },
+  {
+    feature: "Personality and Strengths Analysis",
+    description: 'Provide insights into personality traits and strengths using psychometric analysis. Align personality results with ideal job roles or industries.'
+  },
+  {
+    feature: "AI curated cover letter",
+    description: 'Generate custom cover letters based on the job description and the user\'s resume. Allow users to tweak the tone (e.g., formal, casual, enthusiastic) for personalization.'
   }]
+
+
   const navigate = useNavigate()
   return (
-    <div className='w-screen h-screen bg-black text-white'>
-      <div className='w-4/5 h-full flex flex-col items-center justify-center border border-white m-auto'>
+    <div className='w-screen h-screen bg-black text-white' >
+      <div className='w-4/5 h-full flex flex-col items-center justify-around border border-white m-auto'>
         <h1 className="rounded-full bg-gradient-to-r from-gray-400 to-white text-transparent bg-clip-text text-shadow p-4 font-bold text-4xl stroke-2 stroke-b" >PrepFlow.ai</h1>
         <h3 className='text-2xl'>One shot preparation for your next Interview</h3>
         <img src="" alt="" />
@@ -27,7 +41,7 @@ function App() {
           <Button className="rounded-full">Sign up</Button>
         </div>
         <Button variant={"outline"} className='rounded-full bg-gradient-to-l from-[#57502C] to-[#AF770A]'>Try out now</Button>
-        <div className='grid-cols-2  grid grid-rows-2'>
+        <div className='grid-cols-2 w-1/2 gap-5   grid grid-rows-2'>
 
           {card_content.map((card, index) => (
             <Card key={index}>
@@ -43,7 +57,7 @@ function App() {
         </div>
         <Button onClick={(() => navigate('/resume'))}>Resume</Button>
       </div>
-    </div>
+    </div >
   )
 }
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,18 +1,46 @@
 import { Button } from "@/components/ui/button"
-import { useNavigate } from "react-router"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
+import axios from "axios"
+import { useState } from "react"
 
 function Resume() {
-    const navigate = useNavigate()
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    async function upload() {
+        if (!file) {
+            alert("Please select a file first!");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("file", file);  // 'resume' must match the backend field name
+
+        try {
+            const res = await axios.post("http://localhost:3000/analyze", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+
+            console.log(res.data);
+        } catch (e) {
+            console.error("Upload failed:", e);
+        }
+    }
+
     return (
         <div className="w-screen h-screen   bg-black text-white flex flex-col " >
             <div className="w-3/5  h-screen m-auto gap-10 bg-black text-white flex flex-col items-center justify-center   " >
                 <h1 className="rounded-full bg-gradient-to-r from-gray-400 to-white text-transparent bg-clip-text text-shadow p-4 font-bold text-4xl stroke-2 stroke-b" >PrepFlow.ai</h1>
                 <Input className="w-3/5 h-1/20 p-2    placeholder:text-xl  text-2x " type="text" placeholder="Job Title" />
                 <Textarea className="w-3/5 h-1/4  placeholder:text-xl    text-2xl p-2" type="text" placeholder="Job Description" />
-                <Input className="w-3/5 h-auto p-2    text-2xl " type="file" placeholder="Job Title" />
-                <Button onClick={(() => navigate('/'))}>Analyze</Button>
+                <Input className="w-3/5 h-auto p-2    text-2xl " type="file" placeholder="Job Title" onChange={handleFileChange} />
+                <Button onClick={()=>upload()}>Analyze</Button>
             </div >
         </div>
     )
